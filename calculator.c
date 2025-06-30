4d4549f992058a654802f22099bdc9dd96142672
DhruvLekhi06 <dhruvlekhi06@gmail.com>
2025-06-30 16:22:50 +0530

    c2: Subtraction function implemented

--- calculator.c
+++ calculator.c
@@ -3,6 +3,16 @@
 int main() {
     int a = 5, b = 3;
     int sum = a + b;
+    int diff = a - b;
+    printf("Addition: %d\n", sum);
+    printf("Subtraction: %d\n", diff);
+    return 0;
+}
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 3;
+    int sum = a + b;
     printf("Addition: %d\n", sum);
     return 0;
 }
