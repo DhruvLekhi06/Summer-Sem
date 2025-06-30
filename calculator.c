e78351c67dd04586074153e2d1cff28ce910ca50
DhruvLekhi06 <dhruvlekhi06@gmail.com>
2025-06-30 16:24:10 +0530

    c4: Multiplication function implemented

--- calculator.c
+++ calculator.c
@@ -5,6 +5,20 @@ int main() {
     int sum = a + b;
     int diff = a - b;
     float div = (float)a / b;
+    int prod = a * b;
+    printf("Addition: %d\n", sum);
+    printf("Subtraction: %d\n", diff);
+    printf("Division: %.2f\n", div);
+    printf("Multiplication: %d\n", prod);
+    return 0;
+}
+#include <stdio.h>
+
+int main() {
+    int a = 5, b = 3;
+    int sum = a + b;
+    int diff = a - b;
+    float div = (float)a / b;
     printf("Addition: %d\n", sum);
     printf("Subtraction: %d\n", diff);
     printf("Division: %.2f\n", div);
